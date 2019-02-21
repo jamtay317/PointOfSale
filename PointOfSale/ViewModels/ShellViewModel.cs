dfc1fd75bdc797d49f7b6bff9fcd953b201e3860
James Tays <james@jaila-files.com>
2019-02-20 23:15:25 -0500

    connecting to service

--- PointOfSale/ViewModels/ShellViewModel.cs
+++ PointOfSale/ViewModels/ShellViewModel.cs
@@ -1,9 +1,34 @@
-﻿using PointOfSale.ViewModels.Bases;
+﻿using System.Collections.ObjectModel;
+using PointOfSale.UsersService;
+using PointOfSale.ViewModels.Bases;
+using Prism.Commands;
 
 namespace PointOfSale.ViewModels
 {
     public class ShellViewModel:ViewModelBase
     {
+        private readonly UserServiceClient _userServiceClient;
         public string Title { get; set; } = "Point Of Sale";
+
+        public ShellViewModel()
+        {
+            _userServiceClient = new UserServiceClient();
+            GetUsersCommand = new DelegateCommand(GetUsers);
+            Users = new ObservableCollection<User>();
+        }
+
+        
+
+        public DelegateCommand GetUsersCommand { get; set; }
+
+        public ObservableCollection<User> Users { get; set; }
+
+
+        private void GetUsers()
+        {
+            var users = _userServiceClient.GetClockedInUsers();
+            Users.Clear();
+            Users.AddRange(users);
+        }
     }
 }
